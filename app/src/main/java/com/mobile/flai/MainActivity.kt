eb917823c4b71a29b3f92551adffe5a3ca6512f4
Mehmet Karadana <karadanam519@gmail.com>
2025-07-22 00:43:23 +0300

    first commit

---
+++ app/src/main/java/com/mobile/flai/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.mobile.flai
+
+import android.app.Application
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+import androidx.navigation.compose.rememberNavController
+import com.mobile.flai.ui.screens.ChatScreen
+import com.mobile.flai.ui.theme.FlaiTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+
+sealed class DestinationScreen(var route: String) {
+    data object chatScreen : DestinationScreen("chat")
+}
+
+@HiltAndroidApp
+class ThyAssistantApp : Application()
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FlaiTheme {
+                AppNavigation()
+            }
+        }
+    }
+
+    @Composable
+    fun AppNavigation() {
+
+        val navController = rememberNavController()
+
+        NavHost(
+            navController = navController,
+            startDestination = DestinationScreen.chatScreen.route
+        ) {
+
+            composable(DestinationScreen.chatScreen.route) {
+                ChatScreen()
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/mobile/flai/ui/screens/ChatScreen.kt
@@ -0,0 +1,8 @@
+package com.mobile.flai.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ChatScreen(){
+
+}
